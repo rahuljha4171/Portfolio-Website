cd0b2595a2ebf9029992ac2cd6480a530a00b445
Rahul <104366355+rahuljha4171@users.noreply.github.com>
2022-12-16 21:13:24 +0530

    Project Update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,7 @@ import ProjectCard from "./ProjectCards";
 
 import PrivateChat from "../../Assets/Projects/PrivateChat.png";
 import Portfolio from "../../Assets/Projects/Portfolio.png";
+import Ecart from "../../Assets/Projects/ecart.png";
 import ExpenseTracker from "../../Assets/Projects/ExpenseTracker.png";
 
 import "./project.css";
@@ -20,11 +21,11 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={6} lg={4} className="project-card">
             <ProjectCard
-              imgPath={PrivateChat}
-              title="Private Chat"
-              description="A Personal Chat Application to share resources and hangout with friends build with react.js, css, and Firebase. Have features which allows user for realtime messaging, image sharing and search user."
-              ghLink="#"
-              demoLink="https://chat-app-rahuljha4171.vercel.app/"
+              imgPath={Ecart}
+              title="E Cart"
+              description="This is a fully functional eCommerce website that uses React.js + MUI and CSS in the front end and NodeJs and express js in backend and MongoDb as Database."
+              ghLink="https://github.com/rahuljha4171/E-cart"
+              demoLink="https://ecart.onrender.com/"
             />
           </Col>
 
@@ -35,6 +36,15 @@ function Projects() {
               description="My personal Portfolio Website build with React and Bootstrap. It is fully responsive website which supports both dark and light mode."
               ghLink="https://github.com/rahuljha4171/Portfolio-Website"
               demoLink="https://rahuljha.vercel.app/"
+            />
+          </Col>
+          <Col md={6} lg={4} className="project-card">
+            <ProjectCard
+              imgPath={PrivateChat}
+              title="Private Chat"
+              description="A Personal Chat Application to share resources and hangout with friends build with react.js, css, and Firebase. Have features which allows user for realtime messaging, image sharing and search user."
+              ghLink="#"
+              demoLink="https://chat-app-rahuljha4171.vercel.app/"
             />
           </Col>
 
