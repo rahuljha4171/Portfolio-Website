5948790fac9e76ce2b4556bf1f118b8b36a75db1
Rahul Jha <imrahuljha@outlook.com>
2023-08-09 13:35:44 +0530

    update packages

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -4,10 +4,10 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 
 function AboutCard() {
   return (
-    <Card className="quote-card-view ">
+    <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0" data-aos="zoom-in">
-          <p style={{ textAlign: "justify" }}>
+          <p style={{ textAlign: "justify"}}>
             Hi Everyone, I am <span className="primary-header">Rahul Jha</span> from Darbhanga, Bihar, India. I am a computer science graduate and a passionate programmer.
             <br />
             <br />
