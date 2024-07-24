e04dcb30278c72214fec4ee0f5f01b411bda0dd8
LeonXue <konxue@gmail.com>
2024-07-23 23:26:21 -0500

    Update ProjectCards.js

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-import { CgWebsite } from 'react-icons/cg'
-import { BsGithub } from 'react-icons/bs'
+// import Button from 'react-bootstrap/Button'
+// import { CgWebsite } from 'react-icons/cg'
+// import { BsGithub } from 'react-icons/bs'
 
 function ProjectCards (props) {
   return (
